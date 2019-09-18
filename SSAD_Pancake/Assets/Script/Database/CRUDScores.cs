31dc41e2116629a51d8ac819a0d623bdb9a4c6ce
aldochu <aldochu@gmail.com>
2019-09-18 21:29:16 +0800

    Changed function name

--- SSAD_Pancake/Assets/Script/Database/CRUDScores.cs
+++ SSAD_Pancake/Assets/Script/Database/CRUDScores.cs
@@ -25,7 +25,7 @@ public class CRUDScores : MonoBehaviour
 
 
 
-    public void RandomlyGenerateQuestion()
+    public void RandomlyGenerateScores()
     {
         StudentScores newScore = new StudentScores();
 

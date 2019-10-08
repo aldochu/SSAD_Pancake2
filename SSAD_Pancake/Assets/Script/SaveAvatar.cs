69b62f5902807fcec717bac0fc12f175622a1253
cedvogt <cedric.vogt@hispeed.ch>
2019-10-08 17:56:57 +0800

    Save avatar to DB

---
+++ SSAD_Pancake/Assets/Script/SaveAvatar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveAvatar : MonoBehaviour
+{
+    CustomizeAvatar head;
+    CustomizeAvatar headGear;
+    CustomizeAvatar body;
+
+    public AddUser AddUserInterface;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        head = GameObject.Find("HeadBtn").GetComponent<CustomizeAvatar>();
+        headGear = GameObject.Find("HeadGearBtn").GetComponent<CustomizeAvatar>();
+        body = GameObject.Find("BodyBtn").GetComponent<CustomizeAvatar>();
+
+        AddUserInterface.getUserAvatar(headGear, head, body);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Save()
+    {
+        int headGearIndex = headGear.GetIndex();
+        int headIndex = head.GetIndex();
+        int bodyIndex = body.GetIndex();
+        AddUserInterface.updateAvatar("" + headGearIndex,
+            "" + headIndex,
+            "" + bodyIndex);
+    }
+}

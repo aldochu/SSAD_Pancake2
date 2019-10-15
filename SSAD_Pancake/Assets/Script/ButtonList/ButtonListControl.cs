16e38b3d275360ebab93b4086599d9c78bb4c32b
dinhkhoathoanganh <dinhkhoathoanganh@gmail.com>
2019-10-15 15:11:45 +0800

    added dynamic button scroll list

---
+++ SSAD_Pancake/Assets/Script/ButtonList/ButtonListControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Unity.Editor;
+using Firebase.Database;
+
+public class ButtonListControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject buttonTemplate;
+
+    private void Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject button = Instantiate(buttonTemplate) as GameObject;
+            button.SetActive(true);
+            button.GetComponent<ButtonListButton>().SetText("Button " + i);
+            button.transform.SetParent(buttonTemplate.transform.parent, false);
+        }
+    }
+}

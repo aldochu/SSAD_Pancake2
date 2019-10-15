49d1a083b8f6360b10dba28e5819218f5a8bc9a7
dinhkhoathoanganh <dinhkhoathoanganh@gmail.com>
2019-10-15 21:47:09 +0800

    added CRUDquestions - failed

--- SSAD_Pancake/Assets/Script/ButtonList/ButtonListControl.cs
+++ SSAD_Pancake/Assets/Script/ButtonList/ButtonListControl.cs
@@ -1,23 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Firebase;
-using Firebase.Unity.Editor;
-using Firebase.Database;
 
 public class ButtonListControl : MonoBehaviour
 {
     [SerializeField]
     private GameObject buttonTemplate;
 
+    [SerializeField]
+    public CRUDquestion crudQuestion;
+
     private int numQuestions = 20;
     private void Start()
     {
+        crudQuestion = GetComponent<CRUDquestion>();
+        crudQuestion.getQuestion("world1", "chap1", "easy", callbackFunc);
+    }
+
+    public void callbackFunc(GetQuestion[] questionList)
+    {
+        
+        Debug.Log("CAll back begin");
+        /*
+        for (int i = 0; i < 50; i++)
+        {
+            Debug.Log("Key: " + questionList[i].UniqueKey);
+            Debug.Log("Question: " + questionList[i].question.question);
+        }
+        */
+
         for (int i = 1; i < numQuestions + 1; i++)
         {
             GameObject button = Instantiate(buttonTemplate) as GameObject;
             button.SetActive(true);
-            button.GetComponent<ButtonListButton>().SetText("Question " + i);
+            button.GetComponent<ButtonListButton>().SetText("Question " + questionList[i].question.question);
             button.transform.SetParent(buttonTemplate.transform.parent, false);
         }
     }
